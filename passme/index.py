225392cedcdc71bf4166ae98e13f9424d252bb95
Katsutoshi Seki <sekika@users.noreply.github.com>
2019-07-08 22:08:08 +0900

    up

--- passme/index.py
+++ passme/index.py
@@ -50,6 +50,7 @@ def main():
 <body>
 <h1>Passme cgi</h1>''', flush=True)
 
+    os.system('python -V')
     list = subprocess.check_output(['passme', 'list']).split()
     print(list)
     print(list[0])

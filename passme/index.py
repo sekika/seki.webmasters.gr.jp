5c36c171911234ce54c63e799695712bafa4435a
Katsutoshi Seki <sekika@users.noreply.github.com>
2019-07-09 03:11:40 +0900

    up

--- passme/index.py
+++ passme/index.py
@@ -81,6 +81,8 @@ def main():
     if site != 'none':
         with open("add.sh", "w", encoding="utf-8") as f:
             f.write('cat << EOF | passme add > /dev/null\n{0}\n{1}\n{2}\n{3}\n\nEOF'.format(site, char, plen, desc))
+        subprocess.check_call(['sh', 'add.sh'])
+        os.remove('add.sh')
         print('{0} 追加'.format(site))
         print('<hr>')
 
@@ -102,6 +104,10 @@ def main():
     print('<h2>サイト一覧</h2>', flush=True)
 
     subprocess.check_call(['sh', 'output.sh'])
+
+    list = subprocess.check_output(['passme', 'list']).decode("utf-8").split()
+    for i in range(len(list)):
+    	list[i] = list[i].strip(",[]'")
 
     print('<ul>')
     for s in list:

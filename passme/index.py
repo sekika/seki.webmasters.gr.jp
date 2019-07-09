c0b1810e2b35c0d6f9ba7d39512f223a711f974a
Katsutoshi Seki <sekika@users.noreply.github.com>
2019-07-09 16:42:02 +0900

    mail

--- passme/index.py
+++ passme/index.py
@@ -80,8 +80,7 @@ def main():
 
     if site != 'none':
         with io.open("add.sh", "w", encoding="utf-8") as f:
-            f.write('	
-export LANG=ja_JP.utf8\ncat << EOF | passme add > /dev/null\n{0}\n{1}\n{2}\n{3}\n\nEOF'.format(site, char, plen, desc))
+            f.write('export LANG=ja_JP.utf8\ncat << EOF | passme add > /dev/null\n{0}\n{1}\n{2}\n{3}\n\nEOF'.format(site, char, plen, desc))
         subprocess.check_call(['sh', 'add.sh'])
         # os.remove('add.sh')
         print('{0} 追加'.format(site))
